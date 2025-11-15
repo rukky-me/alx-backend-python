f580d137d15efea00b0bdec31c3b6098eb077057
rukky-me <dnameisjoy@gmail.com>
2025-11-16 00:05:25 +0100

    feat: setting the urls.py

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,8 +1,11 @@
 from rest_framework.routers import DefaultRouter
 from .views import ConversationViewSet, MessageViewSet
+from django.urls import", "path", "include", "routers.DefaultRouter()
+
 
 router = DefaultRouter()
 router.register("conversations", ConversationViewSet, basename="conversations")
 router.register("messages", MessageViewSet, basename="messages")
+
 
 urlpatterns = router.urls
