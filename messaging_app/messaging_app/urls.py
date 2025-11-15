392e1a16dbfb53d37e41b1208e28befc388f09ab
rukky-me <dnameisjoy@gmail.com>
2025-11-16 00:47:27 +0100

    feat: setting the urls.py

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# messaging_app/urls.py
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # API routes
+    path('api/', include('chat.urls')),   # <--- IMPORTANT
 ]
