f8a5d817167b4306aa874150d63a4e3f3b15aff5
rukky-me <dnameisjoy@gmail.com>
2025-11-16 01:01:40 +0100

    feat : setting the urls

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # API routes
-    path('api/', include('chats.urls')),   # <--- IMPORTANT
+    path('api/', include('chats.urls')),   
 ]
