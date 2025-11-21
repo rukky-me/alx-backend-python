55a7526fa82167c2150271c0acb73570c0d3bbc4
rukky-me <dnameisjoy@gmail.com>
2025-11-21 13:47:07 +0100

    Creating a middleware that logs each user’s requests to a file

---
+++ Django-Middleware-0x03/chats/middleware.py
@@ -0,0 +1,24 @@
+import logging
+from datetime import datetime
+
+# Configure logger
+logger = logging.getLogger(__name__)
+handler = logging.FileHandler('request_logs.log')   # Log file will appear in project root
+formatter = logging.Formatter('%(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+
+class RequestLoggingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        user = request.user.username if request.user.is_authenticated else "Anonymous"
+
+        log_message = f"{datetime.now()} - User: {user} - Path: {request.path}"
+        logger.info(log_message)
+
+        response = self.get_response(request)
+        return response
