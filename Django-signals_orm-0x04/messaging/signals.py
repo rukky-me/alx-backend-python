1b93c92086b058ee1d1863a7dd49c56976eb107d
rukky-me <dnameisjoy@gmail.com>
2025-11-24 15:44:51 +0100

    feat: implementing signals for user notifications

---
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -0,0 +1,13 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Message, Notification
+
+@receiver(post_save, sender=Message)
+def create_notification_on_message(sender, instance, created, **kwargs):
+    if created:
+        # Create a notification for the receiver
+        Notification.objects.create(
+            user=instance.receiver,
+            message=instance
+        )
+        print(f"Notification created for {instance.receiver} for message {instance.id}")
