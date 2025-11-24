7f68ba175a30ef32f5075cd1a0b00507662d1d36
rukky-me <dnameisjoy@gmail.com>
2025-11-24 16:18:38 +0100

    feacreating a signal for logging message

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Message, Notification
+from .models import Message, MessageHistory, Notification
 
 @receiver(post_save, sender=Message)
 def create_notification_on_message(sender, instance, created, **kwargs):
@@ -11,3 +11,21 @@ def create_notification_on_message(sender, instance, created, **kwargs):
             message=instance
         )
         print(f"Notification created for {instance.receiver} for message {instance.id}")
+        
+
+@receiver(pre_save, sender=Message)
+def log_message_edit(sender, instance, **kwargs):
+    # Only run if message already exists (edit)
+    if instance.pk:
+        old_message = Message.objects.get(pk=instance.pk)
+
+        # Content changed?
+        if old_message.content != instance.content:
+            # Mark edited
+            instance.edited = True
+
+            # Save old version in history table
+            MessageHistory.objects.create(
+                message=instance,
+                old_content=old_message.content
+            )
