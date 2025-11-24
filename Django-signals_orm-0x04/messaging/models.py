f4af452c678b4b409d2283a65c3c46c08773188f
rukky-me <dnameisjoy@gmail.com>
2025-11-24 16:18:38 +0100

    feacreating a signal for logging message

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -8,6 +8,7 @@ class Message(models.Model):
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="received_messages")
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+    edited = models.BooleanField(default=False)
 
     def __str__(self):
         return f"Message from {self.sender} to {self.receiver}"
@@ -20,3 +21,12 @@ class Notification(models.Model):
 
     def __str__(self):
         return f"Notification for {self.user} - Message ID {self.message.id}"
+    
+class MessageHistory(models.Model):
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='history')
+    old_content = models.TextField()
+    edited_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"History for Message {self.message.id} at {self.edited_at}"
+    
