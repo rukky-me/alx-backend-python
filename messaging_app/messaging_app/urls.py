947400701b7a36e1b7761c1c7bcf0274765713a7
rukky-me <dnameisjoy@gmail.com>
2026-01-12 17:52:29 +0100

    setting up kubernets ingress

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+"""
 # messaging_app/urls.py
 from django.contrib import admin
 from django.urls import path, include
@@ -31,7 +33,30 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-    path('api/', include('chats.urls')),           # the app route
+    path('api/', include('chats.urls')),  # the app route
+    path('api/', include('messaging.urls')),  # the app route
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
+]
+"""
+
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+router = DefaultRouter()
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # JWT token endpoints
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # App URLs
+    path('', include('chats.urls')),
+    path('', include('messaging.urls')),
+
+    path('', include(router.urls)),
 ]
